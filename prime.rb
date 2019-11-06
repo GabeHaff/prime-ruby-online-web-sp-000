a996fc5016cdd30d78e62a42c1277b8313471f8c
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-11-06 18:01:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+prime?(integer)
+if integer<=1 
+  return false 
+end 
+numbers=(2..-1).to_a 
+numbers.each do |integer|
+
+if number % integer == 1 || number % 1 == integer
+  return true 
+else 
+  return false 
+end 
+end 
