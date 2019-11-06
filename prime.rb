5c3b8e606c62dabfe9b30c56b19b4a896f7c2823
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-11-06 18:05:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,3 +11,26 @@ else
   return false 
 end 
 end 
+def invalid_number(number)
+  number == 0 || number == 1 || number != number.abs
+end
+ 
+ 
+def is_prime(number)
+ 
+  return true if number == 2
+ 
+  x = 2
+ 
+  prime_check = number % x
+ 
+  while prime_check > 0
+    x = x + 1
+    return true if x == number
+    prime_check = number % x
+  end
+ 
+  return false
+ 
+end
+ 
